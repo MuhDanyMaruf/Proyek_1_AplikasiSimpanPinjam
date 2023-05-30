4b170074209ccb1bed4a1008fb68f2170ac07a88
MuhDanyMaruf <114630218+MuhDanyMaruf@users.noreply.github.com>
2023-05-30 20:43:07 +0700

    SimpanPinjam

---
+++ column/koneksi.php
@@ -0,0 +1,3 @@
+<?php
+	mysql_connect("localhost","root","") or die ("Koneksi Gagal");
+	mysql_select_db("koperasi") or die ("Database Tidak Terakses");
---
+++ readme.txt
@@ -0,0 +1,6 @@
+Contact Us
+
+email	: andihatmoko@gmail.com
+phone	: 0857 1405 7686
+
+web	: www.rajaputramedia.com
